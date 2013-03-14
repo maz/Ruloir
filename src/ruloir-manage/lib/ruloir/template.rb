33a9f37b444ade4f344a0e0b7994e09d68dc4562
Maz <ma.mazmaz@gmail.com>
2013-03-13 20:44:40 -0400

    doing more work with templates

--- src/ruloir-manage/lib/ruloir/template.rb
+++ src/ruloir-manage/lib/ruloir/template.rb
@@ -1,13 +1,33 @@
 module Ruloir
   class Template
-    DYNAMIC_REGEX=/<%=[ \t\r\n]*([A-Za-z_$0-9@!]+)[ \t\r\n]*(\[[ \t\r\n]*([A-Za-z_$0-9@!]+)[ \t\r\n]*\])?[ \t\r\n]*%>/m
+    DYNAMIC_REGEX_GROUPS=/<%=[ \t\r\n]*([A-Za-z_$0-9@!]+)[ \t\r\n]*(\[[ \t\r\n]*([A-Za-z_$0-9@!]+)[ \t\r\n]*\])?[ \t\r\n]*%>/m
+    DYNAMIC_REGEX_NO_GROUPS=/<%=[ \t\r\n]*[A-Za-z_$0-9@!]+[ \t\r\n]*\[[ \t\r\n]*[A-Za-z_$0-9@!]+[ \t\r\n]*\]?[ \t\r\n]*%>/m 
     
     def self.parse_file(file)
       self.parse(File.read(file))
     end
     def self.parse(code)
-      static_chunks=code.split(DYNAMIC_REGEX)
-      puts code.scan(DYNAMIC_REGEX).inspect
+      self.new(
+        code.split(DYNAMIC_REGEX_NO_GROUPS),
+        code.scan(DYNAMIC_REGEX_GROUPS).map {|x|
+          (x.last==x.first) ? [x.first] : [x.first, x.last]
+        }
+      )
+    end
+    
+    def initialize(static_chunks, dynamic_chunks)
+      @static_chunks=static_chunks
+      @dynamic_chunks=dynamic_chunks
+    end
+    
+    def c_code
+      code="int *len; const char* data;"
+      
+      (0..(@static_chunks.length+@dynamic_chunks.length-1)).each do |i|
+        (((i&1)==0) ? @static_chunks : @dynamic_chunks)[i/2]
+      end
+      
+      code
     end
   end
 end
